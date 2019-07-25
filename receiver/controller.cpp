c099092a87f723f3b859a8d6168bc1f429476818
larissabreu <49702165+larissabreu@users.noreply.github.com>
2019-07-25 15:32:40 -0400

    Write (certo)

--- receiver/controller.cpp
+++ receiver/controller.cpp
@@ -12,12 +12,13 @@ void Controller:: write(int16_t x, int16_t y){
     }
     if( y >= 512){
       aux.left = (aux.left * map(x, 512, 1023, 50, 0))/50;
-    }
+    } 
+    
   }else if( x >= 512){
-    if( y < 512){
+    if( y > 512){
       aux.rigth = (aux.rigth * map(x, 0, 512, 0, 50))/50;
     }
-    if( y >= 512){
+    if( y <= 512){
       aux.rigth = (aux.rigth * map(x, 512, 1023, 50, 0))/50;
     }
   }
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -6,6 +6,8 @@
 #include "controller.h"
 Controller control;
 RH_ASK driver(4000);
+
+
 void setup() {
     control.attach(2,3); 
     Serial.begin(9600);
@@ -14,11 +16,14 @@ void setup() {
 }
 
 void loop() {
+    static uint16_t diff = 0;
+    static uint16_t timer = millis();
     RemoteProtocolHandler data;// Classe usada para manipular os bits recebidos
     uint8_t buffer[4];// Armazena a mensagem recebida    
     uint8_t buf_length = 4;// Armazena a mensagem recebida    
 
     if (driver.recv(buffer, &buf_length)) {
+        timer = millis();
         data.read_from_byte_array(buffer);// interpreta os bits recebidos 
         Serial.print("X: ");
         Serial.print(data.get_x(), DEC);
@@ -26,4 +31,10 @@ void loop() {
         Serial.println(data.get_y(), DEC);
         control.write(data.get_x(), data.get_y());
     }
+    diff = millis() - timer;
+    if ( diff > 200) {
+         diff = 0;
+         control.write(512,512);
+    }
+    
 }
