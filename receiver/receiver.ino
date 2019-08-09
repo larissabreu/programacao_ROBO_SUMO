df7ed63fffdeb22980f6b4646bf8f5646dc3d89d
larissabreu <49702165+larissabreu@users.noreply.github.com>
2019-08-09 08:56:34 -0400

    Revert "memememe"
    This reverts commit 93db25487486c41a39ebdca88e9e064cada420f5.

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,39 +1,40 @@
 #include <ServoTimer2.h>
-#include <RH_NRF24.h>
+#include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
 #include "remote_data.hpp"
 #include "motor.h"
 #include "controller.h"
-
-#define DEBUG
-
 Controller control;
-RH_NRF24 driver;
+RH_ASK driver(4000);
+
+
 void setup() {
     control.attach(2,3); 
-#ifdef DEBUG
     Serial.begin(9600);
     if (!driver.init())
          Serial.println("init failed");
-#else
-    driver.init()
-#endif
 }
 
 void loop() {
+    static uint16_t diff = 0;
+    static uint16_t timer = millis();
     RemoteProtocolHandler data;// Classe usada para manipular os bits recebidos
     uint8_t buffer[4];// Armazena a mensagem recebida    
     uint8_t buf_length = 4;// Armazena a mensagem recebida    
 
     if (driver.recv(buffer, &buf_length)) {
+        timer = millis();
         data.read_from_byte_array(buffer);// interpreta os bits recebidos 
-#ifdef DEBUG
         Serial.print("X: ");
         Serial.print(data.get_x(), DEC);
         Serial.print(" Y: ");
         Serial.println(data.get_y(), DEC);
-#endif
-        if(data.is_valid())
-            control.write(data.get_x(), data.get_y());
+        control.write(data.get_x(), data.get_y());
     }
+    diff = millis() - timer;
+    if ( diff > 200) {
+         diff = 0;
+         control.write(512,512);
+    }
+    
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,4 +1,4 @@
-#include <RH_NRF24.h>
+#include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 #include "remote_data.hpp"
 
@@ -7,7 +7,8 @@
 const int analog_x = A0;
 const int analog_y = A1;
 const int vout = 7;
-RH_NRF24 driver;
+
+RH_ASK driver(4000);
 
 void setup() {
     pinMode(vout, OUTPUT);
@@ -20,6 +21,7 @@ void setup() {
 #else
         driver.init();
 #endif
+    digitalWrite(7,1);// gambiarra pq o corno do jamal fodeu com a porta de 5v
 }
 
 void loop() {
